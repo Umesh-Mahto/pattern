b66eae91b7de9f5344c1baec789599cd8f278457
Umesh <umeshkr846@gmail.com>
2023-08-18 22:41:07 +0530

    ok

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+///ques 12 :
+
+public class Main
+{
+    public static void main(String[] args) {
+        int n=5;
+        int star=1;
+        int space=n-1;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<space;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=star;j++){
+                if(j%2==0){
+                    System.out.print("!");
+                }else{
+                    System.out.print("*");
+                }
+            }
+            space--;
+            star+=2;
+            System.out.println();
+        }
+    }
+}
